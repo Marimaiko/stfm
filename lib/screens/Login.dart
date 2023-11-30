409bff7843c7e669e36940034790a94d0bb3c8ad
Mariana Maiko Hida Tsukasaki <marianamaikoht@gmail.com>
2023-11-30 13:29:03 -0300

    Login

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -13,7 +13,122 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorsPattern.black,
-      appBar: AppBar(title: Text("ncksdnfkj")),
+      // backgroundColor: Colors.amber,
+      body: Padding(
+        padding: const EdgeInsets.only(left: 24.0),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0.0,
+              right: -30.0,
+              child: Image.asset(
+                "assets/decoration.png",
+                height: 300,
+                width: 200,
+                // alignment: Alignment.bottomLeft,
+              ),
+            ),
+            Form(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/logo_stfm.png",
+                      // height: 128,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: ColorsPattern.gold),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: ColorsPattern.gold)),
+                        label: Text("Usuário"),
+                        labelStyle: TextStyle(
+                            color: ColorsPattern.gold,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      style: const TextStyle(color: ColorsPattern.gold),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: ColorsPattern.gold),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: ColorsPattern.gold)),
+                        label: Text("Senha"),
+                        labelStyle: TextStyle(
+                            color: ColorsPattern.gold,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      obscureText: true,
+                      style: const TextStyle(color: ColorsPattern.gold),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    Container(
+                      width: 300,
+                      height: 50,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          side: MaterialStateProperty.all(
+                            const BorderSide(
+                                color: ColorsPattern.gold, width: 1.0),
+                          ),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(24.0))),
+                        ),
+                        child: const Text(
+                          "Entrar",
+                          style: TextStyle(color: ColorsPattern.gold),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Esqueceu a senha?",
+                          style: TextStyle(color: ColorsPattern.gold)),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: 180,
+                      height: 50,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          side: MaterialStateProperty.all(
+                            const BorderSide(
+                                color: ColorsPattern.gold, width: 1.0),
+                          ),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(24.0))),
+                        ),
+                        child: const Text(
+                          "Primeiro Acesso",
+                          style: TextStyle(color: ColorsPattern.gold),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
